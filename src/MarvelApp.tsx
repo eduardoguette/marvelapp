d92e6cca0d879dd33fcd4c5c6a7dae5f366d8cc2
Eduardo Guette <eduardoguette@gmail.com>
2024-06-22 13:18:23 +0200

    feature: Add footer

--- src/MarvelApp.tsx
+++ src/MarvelApp.tsx
@@ -1,14 +1,14 @@
-import { Header, Main } from '@/layout'
+import { Footer, Header, Main } from '@/layout'
 import { Outlet } from 'react-router-dom'
-
 
 export default function MarvelApp() {
   return (
-    <> 
-      <Header/>
+    <>
+      <Header />
       <Main>
         <Outlet />
       </Main>
+      <Footer />
     </>
   )
 }
---
+++ src/layout/Footer/index.tsx
@@ -0,0 +1,8 @@
+import styles from './footer.module.css'
+export const Footer = () => {
+  return (
+    <footer className={styles.footerContainer}>
+      <span className={styles.text}>By Eduardo Guette</span>
+    </footer>
+  )
+}
